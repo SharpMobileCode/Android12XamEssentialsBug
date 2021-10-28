c166830b9d44936ecb344fb91e41a6cb11c9710b
Ruben Macias <SharpMobileCode@users.noreply.github.com>
2021-10-28 11:20:45 -0500

    Initial commit

---
+++ Android12EssentialsBug/Android12EssentialsBug/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Android12EssentialsBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var locationPermissionStatus = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            if (locationPermissionStatus == PermissionStatus.Granted)
+            {
+                ActualResultLabel.Text = PermissionStatus.Granted.ToString();
+            }
+            else
+            {
+                locationPermissionStatus = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                ActualResultLabel.Text = locationPermissionStatus.ToString();
+            }
+        }
+    }
+}
